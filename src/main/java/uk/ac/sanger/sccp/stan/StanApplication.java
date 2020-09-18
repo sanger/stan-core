dd13fffcec9b05b379a3f45fbad3d30aab9bd94e
Chris Smith <cs24@sanger.ac.uk>
2020-09-18 13:31:29 +0100

    First commit

---
+++ src/main/java/uk/ac/sanger/sccp/stan/StanApplication.java
@@ -0,0 +1,13 @@
+package uk.ac.sanger.sccp.stan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/ac/sanger/sccp/stan/StanApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.sanger.sccp.stan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
