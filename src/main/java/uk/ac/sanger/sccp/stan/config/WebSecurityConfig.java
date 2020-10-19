b80cd6fabd2ec06cc4d5a10fa36e65a097c77e18
Chris Smith <cs24@sanger.ac.uk>
2020-10-19 15:50:24 +0100

    Configure CSRF token to be returned in a cookie

--- src/main/java/uk/ac/sanger/sccp/stan/config/WebSecurityConfig.java
+++ src/main/java/uk/ac/sanger/sccp/stan/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 /**
  * @author dr6
@@ -22,7 +23,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/graphql").permitAll()
-                .antMatchers("/graphiql").permitAll();
+                .antMatchers("/graphiql").permitAll()
+            .and()
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
     }
 
 
