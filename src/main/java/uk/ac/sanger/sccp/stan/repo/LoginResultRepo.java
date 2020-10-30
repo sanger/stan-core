c0f9b98c78ad642fb8482086f81546526a670d3c
David Robinson <14000840+khelwood@users.noreply.github.com>
2020-10-30 11:46:31 +0000

    Removed LoginResultRepo, created by accident

--- src/main/java/uk/ac/sanger/sccp/stan/repo/LoginResultRepo.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.sanger.sccp.stan.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import uk.ac.sanger.sccp.stan.model.LoginResult;
-
-public interface LoginResultRepo extends CrudRepository<LoginResult, Integer> {
-}
