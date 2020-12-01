2a9f384dd8c5a56a959e5dad7b595884fd841746
David Robinson <14000840+khelwood@users.noreply.github.com>
2020-12-01 12:27:43 +0000

    Fix mismatches between PlanOperation entities and graphql schema
    Also more thoroughly test graphql serialisation in integration tests.

--- src/test/java/uk/ac/sanger/sccp/stan/IntegrationTests.java
+++ src/test/java/uk/ac/sanger/sccp/stan/IntegrationTests.java
@@ -6,15 +6,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import uk.ac.sanger.sccp.stan.model.LifeStage;
-import uk.ac.sanger.sccp.stan.model.Sample;
+import uk.ac.sanger.sccp.stan.model.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Non-exhaustive integration tests.
@@ -39,7 +37,7 @@ public class IntegrationTests {
         String mutation = tester.readResource("graphql/register.graphql");
         Map<String, Map<String, Map<String, List<Map<String, String>>>>> result = tester.post(mutation);
         assertNotNull(result.get("data").get("register").get("labware").get(0).get("barcode"));
-        assertEquals(result.get("data").get("register").get("tissue").get(0).get("externalName"), "TISSUE1");
+        assertEquals("TISSUE1", result.get("data").get("register").get("tissue").get(0).get("externalName"));
     }
 
     @Test
@@ -47,10 +45,40 @@ public class IntegrationTests {
     public void testPlanOperation() throws Exception {
         tester.setUser(entityCreator.createUser("dr6"));
         Sample sample = entityCreator.createSample(entityCreator.createTissue(entityCreator.createDonor("DONOR1", LifeStage.adult), "TISSUE1"), null);
-        entityCreator.createBlock("STAN-B70C", sample);
+        Labware sourceBlock = entityCreator.createBlock("STAN-B70C", sample);
         String mutation = tester.readResource("graphql/plan.graphql");
         mutation = mutation.replace("$sampleId", String.valueOf(sample.getId()));
-        Map<String, Map<String, Map<String, List<Map<String, String>>>>> result = tester.post(mutation);
-        assertNotNull(result.get("data").get("plan").get("labware").get(0).get("barcode"));
+        Map<String, ?> result = tester.post(mutation);
+        assertNull(result.get("errors"));
+        Object resultPlan = chainGet(result, "data", "plan");
+        List<?> resultLabware = chainGet(resultPlan, "labware");
+        assertEquals(1, resultLabware.size());
+        assertNotNull(chainGet(resultLabware, 0, "barcode"));
+        assertEquals("Tube", chainGet(resultLabware, 0, "labwareType", "name"));
+        List<?> resultOps = chainGet(resultPlan, "operations");
+        assertEquals(resultOps.size(), 1);
+        assertEquals("Section", chainGet(resultOps, 0, "operationType", "name"));
+        List<?> resultActions = chainGet(resultOps, 0, "planActions");
+        assertEquals(1, resultActions.size());
+        Map<String, ?> resultAction = chainGet(resultActions, 0);
+        Map<String, Integer> firstAddress = Map.of("row", 1, "column", 1);
+        assertEquals(firstAddress, chainGet(resultAction, "source", "address"));
+        assertEquals(sourceBlock.getId(), chainGet(resultAction, "source", "labwareId"));
+        assertEquals(firstAddress, chainGet(resultAction, "destination", "address"));
+        assertNotNull(chainGet(resultAction, "destination", "labwareId"));
+        assertEquals(sample.getId(), chainGet(resultAction, "sample", "id"));
+        assertNotNull(chainGet(resultAction, "newSection"));
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> T chainGet(Object container, Object... accessors) {
+        for (Object accessor : accessors) {
+            if (accessor instanceof Integer) {
+                container = ((List<?>) container).get((int) accessor);
+            } else {
+                container = ((Map<?,?>) container).get(accessor);
+            }
+        }
+        return (T) container;
     }
 }
