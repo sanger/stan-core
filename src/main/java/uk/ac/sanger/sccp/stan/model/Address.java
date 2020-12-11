f3f3a40166445b70a1e0c28ece85bb09023a16d8
Dave <14000840+khelwood@users.noreply.github.com>
2020-12-11 09:32:12 +0000

    Refactor: represent Address as a custom GraphQL scalar type (#11)

--- src/main/java/uk/ac/sanger/sccp/stan/model/Address.java
+++ src/main/java/uk/ac/sanger/sccp/stan/model/Address.java
@@ -5,10 +5,12 @@ import org.jetbrains.annotations.NotNull;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.util.Comparator;
+import java.util.Objects;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
+ * An address representing a row and a column (both positive ints).
  * @author dr6
  */
 @SuppressWarnings("JpaDataSourceORMInspection")
@@ -24,14 +26,8 @@ public class Address implements Comparable<Address> {
     public Address() {}
 
     public Address(int row, int column) {
-        if (row < 1) {
-            throw new IllegalArgumentException("Row cannot be less than 1.");
-        }
-        if (column < 1) {
-            throw new IllegalArgumentException("Column cannot be less than 1.");
-        }
-        this.row = row;
-        this.column = column;
+        setRow(row);
+        setColumn(column);
     }
 
     public int getRow() {
@@ -43,19 +39,78 @@ public class Address implements Comparable<Address> {
     }
 
     public void setRow(int row) {
+        if (row < 1) {
+            throw new IllegalArgumentException("Address row cannot be less than 1.");
+        }
         this.row = row;
     }
 
     public void setColumn(int column) {
+        if (column < 1) {
+            throw new IllegalArgumentException("Address column cannot be less than 1.");
+        }
         this.column = column;
     }
 
+    /**
+     * Returns a string representation of this address.
+     * <p>Formats:
+     * <ul>
+     *     <li><tt>"B13"</tt> for row 2, column 13 (if {@code row <= 26})</li>
+     *     <li><tt>"32,15"</tt> for row 32, column 15 (if {@code row > 26}</li>
+     * </ul>
+     * @return a string representation of this address
+     */
     @Override
     public String toString() {
         if (row <= 26) {
             return String.format("%c%d", 'A'+row-1, column);
         }
         return String.format("%d,%d", row, column);
+    }
+
+    /**
+     * Parses a string as an address.
+     * Row and column must be positive ints.
+     * <p>Supported formats:
+     * <ul>
+     *     <li><tt>"B13"</tt>: row 2, column 13</li>
+     *     <li><tt>"32,15"</tt>: row 32, column 15</li>
+     * </ul>
+     * @param string the string to parse
+     * @return the address parsed from the string
+     * @exception NullPointerException if the string is null
+     * @exception IllegalArgumentException if the string is not parsable as an address
+     */
+    public static Address valueOf(String string) {
+        // Adapted from CGAP LIMS
+        Objects.requireNonNull(string, "Cannot convert null to an address.");
+        int row = -1, column = -1;
+        if (string.length() >= 2) {
+            char ch = string.charAt(0);
+            if (ch >= 'A' && ch <= 'Z') {
+                row = ch - 'A' + 1;
+                try {
+                    column = Integer.parseInt(string.substring(1));
+                } catch (NumberFormatException e) {
+                    column = -1;
+                }
+            } else if (ch >= '0' && ch <= '9') {
+                int n = string.indexOf(',');
+                if (n > 0) {
+                    try {
+                        row = Integer.parseInt(string.substring(0, n));
+                        column = Integer.parseInt(string.substring(n + 1));
+                    } catch (NumberFormatException e) {
+                        row = column = -1;
+                    }
+                }
+            }
+        }
+        if (row < 0 || column < 0) {
+            throw new IllegalArgumentException("Invalid address string: "+string);
+        }
+        return new Address(row, column);
     }
 
     @Override
