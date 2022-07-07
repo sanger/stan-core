56c8367a1ad539fb5111576e86d0177a14604c75
Ben Topping <ben.topping@outlook.com>
2022-07-07 15:33:23 +0100

    added javadoc comments to SampleProcessingService

--- src/main/java/uk/ac/sanger/sccp/stan/service/SampleProcessingService.java
+++ src/main/java/uk/ac/sanger/sccp/stan/service/SampleProcessingService.java
@@ -4,6 +4,16 @@ import uk.ac.sanger.sccp.stan.model.User;
 import uk.ac.sanger.sccp.stan.request.AddExternalIDRequest;
 import uk.ac.sanger.sccp.stan.request.OperationResult;
 
+/**
+ * A service to deal with sample processing
+ * @author bt8
+ */
 public interface SampleProcessingService {
-    public OperationResult addExternalID(User user, AddExternalIDRequest request) throws ValidationException;
+    /**
+     * Adds an external id to a samples tissue
+     * @param user the user responsible for the request
+     * @param request the request to record add external id
+     * @return the operations and labware
+     */
+    OperationResult addExternalID(User user, AddExternalIDRequest request) throws ValidationException;
 }
