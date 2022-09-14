d17607d2db45dba7adce038747dc06d6d893d4dd
David <14000840+khelwood@users.noreply.github.com>
2022-09-14 13:44:29 +0100

    remove irrelevant import

--- src/main/java/uk/ac/sanger/sccp/stan/service/releasefile/ReleaseFileService.java
+++ src/main/java/uk/ac/sanger/sccp/stan/service/releasefile/ReleaseFileService.java
@@ -1,6 +1,5 @@
 package uk.ac.sanger.sccp.stan.service.releasefile;
 
-import org.apache.tomcat.jni.Library;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
