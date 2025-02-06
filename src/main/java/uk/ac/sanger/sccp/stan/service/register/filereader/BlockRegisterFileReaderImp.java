dc64a7ab2f792ef1c1237b537bcf8087d7f0dcc1
David Robinson <14000840+khelwood@users.noreply.github.com>
2025-02-06 15:39:18 +0000

    x1288 multiple work numbers (comma sep) in block register file

--- src/main/java/uk/ac/sanger/sccp/stan/service/register/filereader/BlockRegisterFileReaderImp.java
+++ src/main/java/uk/ac/sanger/sccp/stan/service/register/filereader/BlockRegisterFileReaderImp.java
@@ -14,6 +14,8 @@ import java.time.LocalDate;
 import java.util.*;
 
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
+import static uk.ac.sanger.sccp.utils.BasicUtils.nullOrEmpty;
 
 /**
  * @author dr6
@@ -31,12 +33,33 @@ public class BlockRegisterFileReaderImp extends BaseRegisterFileReader<RegisterR
         List<BlockRegisterRequest> blockRequests = rows.stream()
                 .map(row -> createBlockRequest(problems, row))
                 .collect(toList());
-        String workNumber = getUniqueString(rows.stream().map(row -> (String) row.get(Column.Work_number)),
-                () -> problems.add("Multiple work numbers specified."));
+        Set<String> workNumbers = getUnique(rows.stream().map(row -> workNumberSet((String) row.get(Column.Work_number))),
+                () -> problems.add("All rows must list the same work numbers."));
         if (!problems.isEmpty()) {
             throw new ValidationException("The file contents are invalid.", problems);
         }
-        return new RegisterRequest(blockRequests, workNumber==null ? List.of() : List.of(workNumber));
+        return new RegisterRequest(blockRequests, nullOrEmpty(workNumbers) ? List.of() : new ArrayList<>(workNumbers));
+    }
+
+    /**
+     * Gets the set of work numbers specified in a row.
+     * Null if none are specified.
+     * @param string the string listing zero, one or more work numbers
+     * @return a nonempty set of work numbers, or null
+     */
+    public static Set<String> workNumberSet(String string) {
+        if (string == null) {
+            return null;
+        }
+        string = string.trim().toUpperCase();
+        if (string.isEmpty()) {
+            return null;
+        }
+        String[] wns = string.replace(',',' ').split("\\s+");
+        Set<String> set = Arrays.stream(wns)
+                .filter(s -> !s.isEmpty())
+                .collect(toSet());
+        return (set.isEmpty() ? null : set);
     }
 
     /**
