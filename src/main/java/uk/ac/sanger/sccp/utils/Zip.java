f57a7b4d66f2959622c1d06ba001b1e1f754eeaa
David Robinson <14000840+khelwood@users.noreply.github.com>
2025-08-13 17:17:10 +0100

    Extend Zip functionality

--- src/main/java/uk/ac/sanger/sccp/utils/Zip.java
+++ src/main/java/uk/ac/sanger/sccp/utils/Zip.java
@@ -1,8 +1,7 @@
 package uk.ac.sanger.sccp.utils;
 
 import java.util.*;
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
+import java.util.function.*;
 import java.util.stream.*;
 
 import static java.util.Objects.requireNonNull;
@@ -50,9 +49,37 @@ public interface Zip<A,B> {
      * @param <C> type of items in the output stream
      */
     <C> Stream<C> map(BiFunction<? super A, ? super B, ? extends C> mapper);
+
+    /**
+     * Maps pairs of items in the streams to a new pair of items given by two combining functions.
+     * @param mapper1 function to map pairs of the original streams to the first item in the combined stream
+     * @param mapper2 function to map pairs of the original streams to the second item in the combined stream
+     * @return a zip to stream combined items
+     * @param <C> the first type of element in the combined stream
+     * @param <D> the second type of element in the combined stream
+     */
+    <C,D> Zip<C,D> map(BiFunction<? super A, ? super B, ? extends C> mapper1,
+                       BiFunction<? super A, ? super B, ? extends D> mapper2);
+
+    /**
+     * Maps each input stream
+     * @param mapper1 mapper for first input stream
+     * @param mapper2 mapper for second input stream
+     * @return zip of combined items
+     * @param <C> first type in combined stream
+     * @param <D> second type in combined stream
+     */
+    <C,D> Zip<C,D> map(Function<? super A, ? extends C> mapper1, Function<? super B, ? extends D> mapper2);
+
+    /**
+     * Filters the pairs in this stream
+     * @param predicate function to determine which pairs to include
+     * @return a zip of included pairs from this zip
+     */
+    Zip<A,B> filter(BiPredicate<? super A,? super B> predicate);
 }
 
-/** Zip implementation */
+/** Zip implementation from two streams */
 class ZipStream<A, B> implements Zip<A, B> {
     private final Stream<? extends A> aStream;
     private final Stream<? extends B> bStream;
@@ -75,24 +102,92 @@ class ZipStream<A, B> implements Zip<A, B> {
 
     @Override
     public <C> Stream<C> map(BiFunction<? super A, ? super B, ? extends C> mapper) {
-        Spliterator<? extends A> aIterator = aStream.spliterator();
-        Spliterator<? extends B> bIterator = bStream.spliterator();
+        Spliterator<? extends A> spliterA = aStream.spliterator();
+        Spliterator<? extends B> spliterB = bStream.spliterator();
 
-        long estimatedSize = Math.min(aIterator.estimateSize(), bIterator.estimateSize());
+        long estimatedSize = Math.min(spliterA.estimateSize(), spliterB.estimateSize());
 
-        final Iterator<A> iterA = Spliterators.iterator(aIterator);
-        final Iterator<B> iterB = Spliterators.iterator(bIterator);
+        final Iterator<A> iterA = Spliterators.iterator(spliterA);
+        final Iterator<B> iterB = Spliterators.iterator(spliterB);
         Iterator<C> cIterator = new ZipIterator<>(iterA, iterB, mapper);
 
         int zipFlags = (
-                aIterator.characteristics()
-                        & bIterator.characteristics()
+                spliterA.characteristics()
+                        & spliterB.characteristics()
                         & ~(Spliterator.DISTINCT | Spliterator.SORTED)
         );
 
-        Spliterator<C> siC = Spliterators.spliterator(cIterator, estimatedSize, zipFlags);
+        Spliterator<C> spliterC = Spliterators.spliterator(cIterator, estimatedSize, zipFlags);
         boolean parallel = (aStream.isParallel() || bStream.isParallel());
-        return StreamSupport.stream(siC, parallel);
+        return StreamSupport.stream(spliterC, parallel);
+    }
+
+    @Override
+    public <C,D> Zip<C,D> map(BiFunction<? super A, ? super B, ? extends C> mapper1,
+                              BiFunction<? super A, ? super B, ? extends D> mapper2) {
+        requireNonNull(mapper1);
+        requireNonNull(mapper2);
+        return new EntryZip<>(map(EntryZip.Entry::new)).map(mapper1, mapper2);
+    }
+
+    @Override
+    public <C, D> Zip<C, D> map(Function<? super A, ? extends C> mapper1, Function<? super B, ? extends D> mapper2) {
+        requireNonNull(mapper1);
+        requireNonNull(mapper2);
+        return new ZipStream<>(aStream.map(mapper1), bStream.map(mapper2));
+    }
+
+    @Override
+    public Zip<A, B> filter(BiPredicate<? super A, ? super B> predicate) {
+        requireNonNull(predicate);
+        return new EntryZip<>(map(EntryZip.Entry::new)).filter(predicate);
+    }
+}
+
+/** Zip implementation from an entry stream */
+class EntryZip<A,B> implements Zip<A, B> {
+    record Entry<A,B>(A a, B b) {}
+
+    private final Stream<Entry<? extends A, ? extends B>> stream;
+
+    EntryZip(Stream<Entry<? extends A, ? extends B>> stream) {
+        this.stream = stream;
+    }
+
+    @Override
+    public void forEach(BiConsumer<? super A, ? super B> action) {
+        requireNonNull(action);
+        stream.forEach(e -> action.accept(e.a(), e.b()));
+    }
+
+    @Override
+    public <C> Stream<C> map(BiFunction<? super A, ? super B, ? extends C> mapper) {
+        requireNonNull(mapper);
+        return stream.map(e -> mapper.apply(e.a(), e.b()));
+    }
+
+    @Override
+    public <C,D> Zip<C,D> map(BiFunction<? super A, ? super B, ? extends C> mapper1,
+                              BiFunction<? super A, ? super B, ? extends D> mapper2) {
+        requireNonNull(mapper1);
+        requireNonNull(mapper2);
+        Stream<Entry<? extends C, ? extends D>> newStream = stream.map(e -> new Entry<>(mapper1.apply(e.a(), e.b()),
+                mapper2.apply(e.a(), e.b())));
+        return new EntryZip<>(newStream);
+    }
+
+    @Override
+    public <C, D> Zip<C, D> map(Function<? super A, ? extends C> mapper1, Function<? super B, ? extends D> mapper2) {
+        requireNonNull(mapper1);
+        requireNonNull(mapper2);
+        Stream<Entry<? extends C, ? extends D>> newStream = stream.map(e -> new Entry<>(mapper1.apply(e.a()), mapper2.apply(e.b())));
+        return new EntryZip<>(newStream);
+    }
+
+    @Override
+    public Zip<A, B> filter(BiPredicate<? super A, ? super B> predicate) {
+        requireNonNull(predicate);
+        return new EntryZip<>(stream.filter(e -> predicate.test(e.a(), e.b())));
     }
 }
 
