2153e666e411631aa9359a1839d01c8835a95f56
David Robinson <14000840+khelwood@users.noreply.github.com>
2026-03-17 11:34:56 +0000

    x1508 Allow "slot address ..." as a name for the section_address column
    because that's what the example Excel file uses

--- src/main/java/uk/ac/sanger/sccp/stan/service/register/filereader/SectionRegisterFileReader.java
+++ src/main/java/uk/ac/sanger/sccp/stan/service/register/filereader/SectionRegisterFileReader.java
@@ -24,7 +24,7 @@ public interface SectionRegisterFileReader extends MultipartFileReader<SectionRe
         External_slide_ID(Pattern.compile("(external )?(slide (barcode|id|identifier)|barcode.*slide)", Pattern.CASE_INSENSITIVE|Pattern.DOTALL)),
         Prebarcode(String.class, Pattern.compile("(xenium( slide)?\\s*|pre)barcode", Pattern.CASE_INSENSITIVE), false),
         Lot(String.class, Pattern.compile("(xenium )?(slide )?lot\\b.*", Pattern.CASE_INSENSITIVE|Pattern.DOTALL), false),
-        Section_address(Pattern.compile("section\\s*address(es)?", Pattern.CASE_INSENSITIVE)),
+        Section_address(Pattern.compile("(section|slot)\\s*address(es)?\\b.*", Pattern.CASE_INSENSITIVE|Pattern.DOTALL)),
         Fixative,
         Embedding_medium,
         Donor_ID,
