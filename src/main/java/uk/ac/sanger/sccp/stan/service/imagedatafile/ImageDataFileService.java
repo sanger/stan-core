eeef08179fe0b264672bf2c65ac50b9021625306
David Robinson <14000840+khelwood@users.noreply.github.com>
2026-04-28 16:08:15 +0100

    x1520 UAT change: Support generating an image qc file for multiple ops
    URL imageqc?id= now accepts multiple ids and generates a file
    combined for all of them.

--- src/main/java/uk/ac/sanger/sccp/stan/service/imagedatafile/ImageDataFileService.java
+++ src/main/java/uk/ac/sanger/sccp/stan/service/imagedatafile/ImageDataFileService.java
@@ -3,12 +3,14 @@ package uk.ac.sanger.sccp.stan.service.imagedatafile;
 import uk.ac.sanger.sccp.stan.model.Operation;
 import uk.ac.sanger.sccp.utils.tsv.TsvFile;
 
+import java.util.Collection;
+
 /** Service for generating file data for image qc ops */
 public interface ImageDataFileService {
     /**
-     * Generate file data containing the image data for the given operation.
-     * @param op the operation
+     * Generate file data containing the image data for the given operations.
+     * @param ops the operations
      * @return the file data
      */
-    TsvFile<?> generateFile(Operation op);
+    TsvFile<?> generateFile(Collection<Operation> ops);
 }
